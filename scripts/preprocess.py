b26ff161e55e4d27d2537b07fc3fe52dc924f89f
Marco Sin (mas634) <marcosin918@gmail.com>
2024-07-09 15:39:27 -0600

    refactor: Refactored parse_json() to place the answers within the 'answers' column of the dataframe

--- scripts/preprocess.py
+++ scripts/preprocess.py
@@ -10,10 +10,13 @@ def helper(row):
     return out
 
 def parse_json(fname:str="../NYT-Connections-Answers/connections.json"):
-    df = pd.read_json(fname)['answers']
+    df = pd.DataFrame()
     
-    return df.apply(lambda x: helper(x))
-
+    df['answers'] = pd.read_json(fname)['answers']
+    df['answers'] = df['answers'].apply(helper)
+    df['answers'] = df['answers'].apply(lambda x: sum(x, []))
+    
+    return df
 
 
 if __name__ == "main":
